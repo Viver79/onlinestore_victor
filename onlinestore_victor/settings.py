0ee439fb2181fd3373564ef49494fa8cd3d3a0dc
Viver79 <73900985+Viver79@users.noreply.github.com>
2021-03-26 01:56:41 +0200

    Update settings.py

--- onlinestore_victor/settings.py
+++ onlinestore_victor/settings.py
@@ -158,7 +158,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
