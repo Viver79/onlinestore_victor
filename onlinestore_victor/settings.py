fe77a33ce336cd76fbf60b17a216558e35eddeea
Viver79 <73900985+Viver79@users.noreply.github.com>
2021-03-26 20:52:18 +0200

    Update settings.py

--- onlinestore_victor/settings.py
+++ onlinestore_victor/settings.py
@@ -22,7 +22,7 @@ DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -167,7 +167,7 @@ else:
         os.path.join(BASE_DIR, 'static'),
     ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' #применить сжатие,не кэширования
+    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' #применить сжатие,не кэширования
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage' #Проверка
 
